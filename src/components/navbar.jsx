ec82c5fd98f61d78e1d7383eee3ec09b863060c8
Carlos López <clopez@migob.gob.ni>
2023-07-25 08:16:34 -0600

    Avance en navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import "../navbar.css";
 
-function Navbar(props) {
+function Navbar() {
+  const[active, setActive] = useState("nav__menu");
+  const[toggleIcon, setToggleIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    active === 'nav__menu' 
+    ? setActive("nav__menu nav__active")
+    : setActive("nav__menu");
+    //animacion 
+    toggleIcon === 'nav__toggler'
+    ? setToggleIcon ("nav__toggler toggle")
+    : setToggleIcon ("nav__toggler")
+  };
   return (
     <div>
       <nav className="nav">
-        <a href="#" className="brand"></a>
-        <ul className="nav__menu">
+        <a href="#" className="brand">
+          EasyShare
+        </a>
+        <ul className={active}>
           <il className="nav__item">
             <a href="#" className="nav__link">
               Home
@@ -17,9 +31,11 @@ function Navbar(props) {
             </a>
           </il>
         </ul>
-        <div className="nav__toggler">
+        <div onClick={navToggle} className={toggleIcon}>
           <div className="line1"></div>
           <div className="line2"></div>
+          <div className="line3"></div>
+
         </div>
       </nav>
     </div>
